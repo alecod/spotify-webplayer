825588906758863d0a3e054bc308b13b471729a6
Alessandro Spoto <alecodx@gmail.com>
2024-04-17 22:01:43 -0300

    sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Sidebar from "../components/Sidebar";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Sidebar />
+    </div>
   )
 }
